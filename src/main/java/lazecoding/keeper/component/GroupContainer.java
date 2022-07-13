78af913b42322975f04af75644fbbc355351caad
lazecoding <15062270668@163.com>
2022-07-14 07:20:41 +0800

    perf:修改为静态变量

--- src/main/java/lazecoding/keeper/component/GroupContainer.java
+++ src/main/java/lazecoding/keeper/component/GroupContainer.java
@@ -19,16 +19,16 @@ public class GroupContainer {
     /**
      * ChannelId 和 ChannelHandlerContext 的映射。
      */
-    public static Map<String, ChannelHandlerContext> CHANNEL_CONTEXT = new ConcurrentHashMap<>();
+    public final static Map<String, ChannelHandlerContext> CHANNEL_CONTEXT = new ConcurrentHashMap<>();
 
     /**
      * ChannelId 和 userTag 的映射。
      */
-    public static Map<String, String> CHANNEL_USER = new ConcurrentHashMap<>();
+    public final static Map<String, String> CHANNEL_USER = new ConcurrentHashMap<>();
 
     /**
      * userTag 和 一组ChannelId 的映射，一个用户可能多端登录。
      */
-    public static Map<String, CopyOnWriteArraySet<String>> USER_CHANNEL = new ConcurrentHashMap<>();
+    public final static Map<String, CopyOnWriteArraySet<String>> USER_CHANNEL = new ConcurrentHashMap<>();
 
 }
